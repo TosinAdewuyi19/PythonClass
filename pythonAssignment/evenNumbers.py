63dc32ee645785546b03fdae99c75484b67d2b12
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-10-23 20:22:20 +0100

    assignment

---
+++ pythonAssignment/evenNumbers.py
@@ -0,0 +1,3 @@
+count = 0
+	for count even number, range(0, 100, 2,)
+print("The even numbers between 1 to 100 is: ")
---
+++ pythonAssignment/radius.py
@@ -0,0 +1,13 @@
+radius = 2
+pi = 3.14159
+
+diameter = 2 * radius
+circumference = 2 * pi * radius
+area = pi * radius ** 2
+
+print("Circle Calculations:")
+print("          ")
+print(f"Radius: {radius}")
+print(f"Diameter: {diameter}")
+print(f"Circumference: {circumference:.2f}")
+print(f"Area: {area:.2f}")
