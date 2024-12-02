7c0cbfa36b0d6d1c92c4adfa3bbadb6855a6f81b
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2024-12-02 14:53:52 +0100

    phasegate

---
+++ phaseGateOne/sumOfGeneratedNmbers.py
@@ -0,0 +1,30 @@
+number1 = 23
+number2 = 20
+
+print("number1 = 23")
+print("number2 = 20")
+number1 = 23
+number2 = 20
+sum = number1 + number2
+	
+answer = str(input("Enter the sum of these numbers: "))
+if answer == sum:
+	print("true")
+if answer != sum:
+	print("False")
+	
+	
+
+"""Pseudocode
+assign didgits to the numbers
+print the numbers
+create sum variable that calculate the sum of the numbers
+prompt the user to enter the sum of the numbers
+display true if the user input is correct and false if it is not."""
+
+
+
+
+
+
+	
